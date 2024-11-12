33ac311245ce9e01282ce987c2be292255150e59
Nagore <nagore.colomer01@estudiant.upf.edu>
2024-11-12 17:23:16 +0100

    ultimisima version

--- firts_seminar.py
+++ firts_seminar.py
@@ -5,6 +5,21 @@ import numpy as np
 import pywt
 
 
+#TASK2 
+class ex2:
+    #creamos el metodo que pasa de RGB a YUV simplemente aplicamo sla formula de teoria
+    def RGB_to_YUV(r, g, b):
+        Y= 0.257*r+0.504*g+0.098*b+16
+        U=-0.148*r-0.291*g+0.439 * b+128
+        V=0.439*r-0.368*g-0.071 *b +128
+        return Y,U,V
+    #creamos el metodo para pasar de YUV a RGB siguiendo la formula de teoria
+    def YUV_to_RGB(y,u,v):
+        B=1.164*(y-16)+2.018*(u-128)
+        G=1.164 * (y-16) - 0.813 * (v-128) - 0.391*(u-128)
+        R=1.164*(y-16)+1.596*(v-128)
+        return R,G,B
+    
 #TASK 3
 from PIL import Image
 class ex3:
